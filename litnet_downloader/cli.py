cf77b1ac4a2576307f91f9c8869a823f69911178
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-04-05 16:44:27 +0300

    add command-line options for CLI

--- litnet_downloader/cli.py
+++ litnet_downloader/cli.py
@@ -1,15 +1,35 @@
-"""CLI for utility."""
+"""CLI application.
+
+auth-token could be obtained from cookie 'litera-frontend'
+
+
+Usage:
+  litnet-downloader <auth-token> <book-url> [--page-delay=<seconds>]
+  litnet-downloader -h | --help | --version
+"""
 
 from sys import exit
+from typing import Any
+
+from docopt import docopt
 
 from litnet_downloader.book_downloader import BookDownloader
+from litnet_downloader.version import __version__
+
+
+def get_option(options: dict[str, Any], key: str, /, *, default: Any = None) -> Any:
+    option = options.get(key, default)
+    return option if option else default
 
 
 def run() -> None:
-    url = 'https://litnet.com/ru/reader/para-dlya-drakona-ili-prosto-letim-domoi-b135045'
-    token = 'look at cookies for value of <litera-frontend>'
+    arguments = docopt(__doc__, version=__version__)
 
-    downloader = BookDownloader(url, token)
+    downloader = BookDownloader(
+        url=arguments['<book-url>'],
+        token=arguments['<auth-token>'],
+        delay_secs=get_option(arguments, '--page-delay', default=1)
+    )
 
     book = downloader.get_book()
     with open(f'{book.title}.txt', 'w', encoding='utf-8') as file:
