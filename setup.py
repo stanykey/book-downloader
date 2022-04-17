de86cdd7b5fde5c6136493939bdebd895b6deaf1
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-04-17 16:02:15 +0300

    add missed instructions into setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 """This module contains setup instructions for litnet_downloader."""
 
+from pathlib import Path
+
 from setuptools import setup
 
 from litnet_downloader.version import __version__
+
+
+def read_requirements() -> list[str]:
+    requirements_file = Path(__file__).parent.resolve() / 'requirements.txt'
+    with open(requirements_file, 'r', encoding='utf-8') as file:
+        requirements = [line.strip() for line in file.readlines()]
+
+    requirements.remove('setuptools')
+    return requirements
 
 
 setup(
     name='litnet_downloader',
     version=__version__,
     author_email='sergey.lovygin@yahoo.com',
+    install_requires=read_requirements(),
     packages=['litnet_downloader'],
     entry_points={
         'console_scripts': [
