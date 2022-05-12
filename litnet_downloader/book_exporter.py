3facb5f4c2ac0b95f2ac75fab5d29c7822f2bd6f
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-05-12 22:26:52 +0300

    fix crash during book dumping/exporting process

--- litnet_downloader/book_exporter.py
+++ litnet_downloader/book_exporter.py
@@ -4,6 +4,7 @@ from typing import Protocol
 
 from litnet_downloader.book_data import BookData
 from litnet_downloader.book_data import ChapterData
+from litnet_downloader.details.misc import ensure_directory_exists
 
 
 class BookFormatter(Protocol):
@@ -23,6 +24,8 @@ class BookExporter:
 
     def dump(self, book: BookData) -> None:
         book_path = self._output_root / self._exporter.filename(book)
+        ensure_directory_exists(book_path.parent)
+
         with open(book_path, "w", encoding="utf-8") as file:
             for chapter in book.chapters:
                 file.write(self._exporter.prepare(chapter))
